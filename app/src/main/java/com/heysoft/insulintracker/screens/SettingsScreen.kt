aafcb15cecb5aef2163bc2de14561bb781c8a7b3
Heyone77 <heyone3131@gmail.com>
2024-06-15 04:34:38 +0500

    Alpha 1.4.2 Добавил в настройки переключение темы

--- app/src/main/java/com/heysoft/insulintracker/screens/SettingsScreen.kt
+++ app/src/main/java/com/heysoft/insulintracker/screens/SettingsScreen.kt
@@ -1,10 +1,47 @@
 package com.heysoft.insulintracker.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun SettingsScreen(modifier: Modifier = Modifier) {
-    Text(text = "Здесь пока ничего нет", modifier = modifier)
+fun SettingsScreen(modifier: Modifier = Modifier, isDarkTheme: MutableState<Boolean>) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Темная тема", fontSize = 18.sp)
+            Switch(
+                checked = isDarkTheme.value,
+                onCheckedChange = { newTheme ->
+                    isDarkTheme.value = newTheme
+                },
+                colors = SwitchDefaults.colors(
+                    checkedThumbColor = MaterialTheme.colorScheme.primary,
+                    uncheckedThumbColor = Color.LightGray
+                )
+            )
+        }
+    }
 }
